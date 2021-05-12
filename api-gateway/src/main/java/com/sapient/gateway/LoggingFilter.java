95e9fee0884e14919715cc1be865d945b738ffec
mishrasub <subhash06582@gmail.com>
2021-05-12 09:40:55 +0530

    API Gateway changes

---
+++ api-gateway/src/main/java/com/sapient/gateway/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.sapient.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("Path and query param of the request received Path --> {}, query --> {}",
+				exchange.getRequest().getPath(), exchange.getRequest().getQueryParams());
+		return chain.filter(exchange);
+	}
+
+}
